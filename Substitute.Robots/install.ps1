f4dcacfce443c75aa5b66bdae7e3b91302d87253
Johan Redborg <johan.redborg@gmail.com>
2012-05-15 11:43:16 +0200

    initial checkin

---
+++ Substitute.Robots/install.ps1
@@ -0,0 +1,49 @@
+﻿# First some common params, delivered by the nuget package installer
+param($installPath, $toolsPath, $package, $project)
+
+# Get the build project of type [Microsoft.Build.Evaluation.Project]
+$buildProject = Get-Project $project.ProjectName | % {
+            $path = $_.FullName
+            @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($path))[0]
+        }
+
+# Get project path
+$path = [System.IO.Path]
+$projectpath = $path::GetDirectoryName($project.FileName)
+
+# Get robots.txt
+$robotstxt = $path::Combine($projectpath, "robots.txt")
+
+# Create robots.txt if it doesnt exist
+If (Test-Path $robotstxt)
+{
+    # robots.txt exists, no file creation needed.
+}
+Else
+{
+    # robots.txt does not exist, creating file.
+
+	# create the new file
+    new-item $robotstxt -type file
+
+	# Add a new ItemGroup
+	$itemGroup = $buildProject.Xml.AddItemGroup()
+
+	# Add the Content node including "robots.txt" to the ItemGroup
+	$itemGroup.AddItem("Content", "robots.txt")
+}
+
+# Find the Content node for "robots.txt.substitute.xml"
+$robotssubstitute = $buildProject.Xml.ItemGroups | foreach {$_.Items} | Where-Object {$_.Include -match "robots.txt.substitute.xml"}
+
+# Add a dependency to "robots.txt.txt" for "robots.txt.substitute.xml"
+$robotssubstitute.AddMetaData("DependentUpon", "robots.txt.txt")
+
+# Save the build project.
+$buildProject.Save()
+
+# Save the project from the params.
+$project.Save()
+
+
+
---
+++ build.boo
@@ -0,0 +1,48 @@
+import System.IO
+
+solution_file = "SubstituteApp.Robots.sln"
+project_dir = "SubstituteApp.Robots"
+nuspecfile = "SubstituteApp.Robots.nuspec"
+build_folder  = "build/"
+nuget_folder  = "nuget/"
+build_config  = env('build.config')
+build_output  = env('build.output')
+
+target default, (compile):
+   pass
+   
+target artifact, (compile, copy_files):
+   pass
+   
+target nuget, (compile, copy_files, nuget_push):
+   pass
+
+target compile:
+   msbuild(file: solution_file, configuration: build_config, version: "4")
+
+target nuget_pack:
+    rmdir(nuget_folder)
+    mkdir(nuget_folder)
+
+    exec("Libraries/Phantom/lib/nuget/NuGet.exe", "pack ${nuspecfile} /o ${nuget_folder}")
+
+target nuget_push, (nuget_pack):
+    exec("Libraries/Phantom/lib/nuget/NuGet.exe", "push ${nuget_folder}SubstituteApp.Robots.1.0.5.nupkg")
+
+
+target prepare_folders:
+   exec("attrib", "-R ${project_dir}/** /S /D")
+   rmdir(build_folder)
+   mkdir(build_folder)
+
+desc "Copy files"
+target copy_files, (prepare_folders):
+  print "Copying files to package"
+
+  with FileList(project_dir):
+    .Include("*.txt.txt")
+    .Include("*.xml")
+    .Include("*.ps1")
+    .Include("*.transform")
+    .ForEach def(file):
+      file.CopyToDirectory(build_folder)
